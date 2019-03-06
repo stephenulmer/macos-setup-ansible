1d0d650d58b5033da8a172288977a44bbc8fa902
Stephen Ulmer <ulmer@ulmer.org>
2019-03-06 10:27:29 -0500

    setup.sh: skip tasks with vault tag by default

--- setup.sh
+++ setup.sh
@@ -19,4 +19,4 @@ curl -L "https://github.com/stephenulmer/${repo}/tarball/master" \
 	| tar -xf - --strip-components 1
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook --skip-tag="mas" -i localhost, --connection=local setup.yml
+ansible-playbook --skip-tags="mas,vault" -i localhost, --connection=local setup.yml
