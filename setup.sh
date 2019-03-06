01ae44139d56a7d97d66249f03311aa390c18198
Stephen Ulmer <ulmer@ulmer.org>
2019-03-06 09:35:35 -0500

    install ansible into bootstrap area and then remove later

--- setup.sh
+++ setup.sh
@@ -4,14 +4,17 @@
 ##
 
 repo="macos-setup-ansible"
+export PYTHONUSERBASE="${HOME}/.ansible-bootstrap"
+export PATH=${PYTHONUSERBASE}/bin:$PATH
 
 sudo -v
 sudo launchctl limit maxfiles unlimited
 sudo easy_install pip
-sudo pip install ansible
+pip install --user ansible
 
 mkdir -p "${repo}"
 cd "${repo}"
+
 curl -L "https://github.com/stephenulmer/${repo}/tarball/master" \
 	| tar -xf - --strip-components 1
 
